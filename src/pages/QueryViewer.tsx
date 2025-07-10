cf08d5943ccff149c22a269e1e9c92625a3165ba
Ethan Pauwels <ethanpauwels@gmail.com>
2025-07-10 17:23:32 -0500

    Fix: use full Render backend URL in query tool

--- src/pages/QueryViewer.tsx
+++ src/pages/QueryViewer.tsx
@@ -14,7 +14,7 @@ export default function QueryViewer() {
     setResult(null);
 
     try {
-      const res = await fetch("/query", {
+      const res = await fetch("https://shellsync.onrender.com/query", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query }),
